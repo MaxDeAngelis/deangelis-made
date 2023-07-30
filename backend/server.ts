4c504fb2f50538449504b3c46e54c1ac0fbfc6b7
katiedeangelis <Maxwell.DeAngelis@gmail.com>
2023-07-30 11:23:41 -0400

    Initial setup of structure

---
+++ backend/server.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import express from "express";
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
