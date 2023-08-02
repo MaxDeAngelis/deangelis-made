6cb9a9b5e8172ef300a154b8748f8375f23ae58b
katiedeangelis <Maxwell.DeAngelis@gmail.com>
2023-08-01 21:00:47 -0400

    Enable prettier and clean up eslint

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,11 @@
-import path from "path";
-import express from "express";
+import path from 'path';
+import express from 'express';
 
 const { PORT = 3001 } = process.env;
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "../../frontend/dist")));
+app.use('/', express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.listen(PORT, () => {
   console.log(`Server listening @ http://localhost:${PORT}`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,27 +8,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer" />
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer' />
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React -2</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is
-          {' '}
-          {count}
+          count is {count}
         </button>
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
