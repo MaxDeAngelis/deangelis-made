e8a267d56e653eb8a0d774628a5c1e663a4ad5e2
katiedeangelis <Maxwell.DeAngelis@gmail.com>
2023-09-02 20:24:49 -0400

    Add search API

--- backend/server.ts
+++ backend/server.ts
@@ -49,6 +49,12 @@ app.get('/api/recipe', (req, res) => {
   }
 });
 
+app.get('/api/search', (req, res) => {
+  Recipe.find({ name: { $regex: req.query.q, $options: 'i' } }).then((docs) => {
+    res.send(docs);
+  });
+});
+
 app.get('/api/recents', (req, res) => {
   Recipe.find()
     .sort({ createdAt: -1 })
