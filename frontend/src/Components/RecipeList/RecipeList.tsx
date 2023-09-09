17175a313db680da63419b7998f2ac8959fb09c1
katiedeangelis <Maxwell.DeAngelis@gmail.com>
2023-09-09 15:35:24 -0400

    Add time metadata to recipe list

--- frontend/src/Components/RecipeList/RecipeList.tsx
+++ frontend/src/Components/RecipeList/RecipeList.tsx
@@ -1,5 +1,12 @@
-import { Container, Row, Info, Icon, Title, Author } from './RecipeList.styles';
-
+import {
+  Container,
+  Row,
+  Info,
+  Icon,
+  Title,
+  Author,
+  Time,
+} from './RecipeList.styles';
 import RecipeListProps from './RecipeList.types';
 
 function RecipeList({ heading, recipes }: RecipeListProps): JSX.Element {
@@ -7,12 +14,22 @@ function RecipeList({ heading, recipes }: RecipeListProps): JSX.Element {
     <>
       <h2>{heading}</h2>
       <Container>
-        {recipes.map(({ id, image, name, url, author }) => (
+        {recipes.map(({ id, image, name, url, prepTime, cookTime, author }) => (
           <Row key={id} href={`/recipe/${url}`}>
             <Icon src={image} />
             <Info>
               <Title>{name}</Title>
               <Author>{author}</Author>
+              <Time>
+                <label htmlFor='prepTime'>
+                  Prep
+                  <span>{`${prepTime.amount} ${prepTime.unit}`}</span>
+                </label>
+                <label htmlFor='cookTime'>
+                  Cook
+                  <span>{`${cookTime.amount} ${cookTime.unit}`}</span>
+                </label>
+              </Time>
             </Info>
           </Row>
         ))}
